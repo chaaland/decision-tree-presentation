74569d2b5fbb3fa5bc48bf195a3ee0700ed1c0a2
Casey Haaland <caseychandlerh@yahoo.com>
2022-09-28 22:02:04 +0100

    add gridlines

--- regions.py
+++ regions.py
@@ -4,12 +4,11 @@ from tree_algo import IMAGE_DIR
 
 
 def main():
-    # create plot of values
     plt.axhline(y=0.5, color="k", linestyle="-", linewidth=4, xmin=0, xmax=1)
     plt.axvline(x=0.25, color="k", linestyle="-", linewidth=4, ymin=0, ymax=0.75)
-    plt.fill_between([-1, 1], y1=0.5, y2=1, color="tab:red", alpha=0.7)
-    plt.fill_between([-1, 0.25], y1=-1, y2=0.5, color="tab:green", alpha=0.7)
-    plt.fill_between([0.25, 1], y1=-1, y2=0.5, color="tab:blue", alpha=0.7)
+    plt.fill_between([-1, 1], y1=0.5, y2=1, color="tab:blue", alpha=0.7)
+    plt.fill_between([-1, 0.25], y1=-1, y2=0.5, color="turquoise", alpha=0.7)
+    plt.fill_between([0.25, 1], y1=-1, y2=0.5, color="dodgerblue", alpha=0.7)
 
     plt.xlim([-1, 1])
     plt.ylim([-1, 1])
@@ -26,7 +25,8 @@ def main():
     plt.tick_params(labelleft=False, left=True, color="white")
     plt.tick_params(labelbottom=False, bottom=True, direction="inout")
     plt.tight_layout()
-    # plt.grid(linestyle="--")
+    # plt.grid(
+    plt.grid(linestyle="--", color="lightgray", alpha=0.5)
     plt.savefig(IMAGE_DIR / "regions.png", transparent=True)
 
 
